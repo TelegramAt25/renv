cda1bbcaa3ca6579140af8a890897dc80f7e8db1
fukiame <fukiame@proton.me>
2025-11-18 20:13:46 +0700

    w
    Signed-off-by: fukiame <fukiame@proton.me>

--- qup.sh
+++ qup.sh
@@ -18,7 +18,7 @@ apt -v && {
     sudo apt update
     sudo apt upgrade -y
     sudo apt install neovim ripgrep git git-lfs imagemagick jq zip unzip rsync -y
-    sudo apt install libncurses5 libncurses5-dev libssl-dev build-essential -y || :
+    sudo apt install libncurses5 libncurses5-dev libssl-dev build-essential python-is-python3 -y || :
 } || :
 
 repo help || {
